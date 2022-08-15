ab5381def2d6d0b001a6400d708f4ffed6d378c2
waliamar <amrutwali@gmail.com>
2022-08-15 10:17:06 +0530

    Added Genre Songs Schema

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
+  console.log(url);
   mongoose.connect(url).then(console.log("connected to DB"));
 };
 
--- models/songs.js
+++ models/songs.js
@@ -15,20 +15,33 @@ const SongSchema = new mongoose.Schema({
     type: Date,
   },
   rating: {
-    // type: mongoose.Mixed,
-    // 1: Number,
-    // 2: Number,
-    // 3: Number,
-    // 4: Number,
-    // 5: Number,
-    // default: { 1: 1, 2: 1, 3: 1, 4: 1, 5: 1 },
-    type: Number,
-    default: 0,
+    type: mongoose.Mixed,
+    1: Number,
+    2: Number,
+    3: Number,
+    4: Number,
+    5: Number,
+    default: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
   },
   url: {
     type: String,
     default:
       "https://cdn.vectorstock.com/i/preview-1x/48/06/image-preview-icon-picture-placeholder-vector-31284806.jpg",
+  },
+  genre: {
+    list: {
+      type: [String],
+      default: [
+        "Electronic Dance Music",
+        "Rock",
+        "Jazz",
+        "Dubstep",
+        "Rhythm and Blues",
+        "Country Music",
+        "Pop Music",
+      ],
+    },
+    set: { type: Number, min: 0, max: 7 },
   },
 });
 
